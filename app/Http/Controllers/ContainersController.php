82cddb4c849cc6158547375c46e48b9a6681293e
ophum <inagaki0106@gmail.com>
2020-01-10 17:37:25 +0900

    ２２番ポートのプロ棋士を自動追加

--- app/Http/Controllers/ContainersController.php
+++ app/Http/Controllers/ContainersController.php
@@ -101,19 +101,46 @@ class ContainersController extends Controller
         ];
 
         $res = $pj->post("http://localhost:8080/containers/create", $body);
-        if($res != "") {
-            $data = [
-                'user_id' => $user->id,
-                'network_id' => $network_id,
-                'name' => $name,
-                'cpu' => $cpu,
-                'memory' => $memory, 
-                'ipv4_address' => $ipv4_address,
-            ];
-
-            $container = new Container();
-            $container->fill($data)->save();
+        if($res == "") {
+            return;
         }
+        $data = [
+            'user_id' => $user->id,
+            'network_id' => $network_id,
+            'name' => $name,
+            'cpu' => $cpu,
+            'memory' => $memory, 
+            'ipv4_address' => $ipv4_address,
+        ];
+
+        $container = new Container();
+        $container->fill($data)->save();
+
+        $network = $container->network;
+        $endpoint_port = 0;
+
+        do {
+            $endpoint_port = strval(rand(1234, 65530));
+            $f = $res = NetworkConfigure::where('endpoint_port', $endpoint_port)->exists();
+        }while($f);
+        
+        $pj = new PostJson();
+        $router_name = str_replace('_', '-', $network->name).'-router';
+        $ipv4 = explode('/', $container->ipv4_address)[0];
+        $pj->post("http://localhost:8080/networks/config/proxy", [
+            'router_name' => $router_name,
+            'endpoint_port' => $endpoint_port,
+            'destination_address' => $ipv4,
+            'destination_port' => '22',
+        ]);
+
+        $nc = new NetworkConfigure();
+        $nc->network_id = $network->id;
+        $nc->container_id = $container->id;
+        $nc->endpoint_port = $endpoint_port;
+        $nc->dport = 22;
+        $nc->save();
+
 
         return redirect('/home');
     }
